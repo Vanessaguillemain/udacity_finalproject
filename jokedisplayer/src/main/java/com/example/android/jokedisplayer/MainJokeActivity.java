33963eea5328548a7a1ae750c93d91c5ebf7d9fa
vanessa <vanessa.guillemain@gmail.com>
2019-04-17 17:54:29 +0200

    Fin Step 2

---
+++ jokedisplayer/src/main/java/com/example/android/jokedisplayer/MainJokeActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.jokedisplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainJokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_joke);
+
+        Intent i = getIntent();
+        String joke = i.getStringExtra("joke");
+        TextView tvJoke = findViewById(R.id.tvJoke);
+        tvJoke.setText(joke);
+    }
+}
---
+++ jokeproviderlib/src/main/java/com/example/android/jokeproviderlib/JokeProvider.java
@@ -0,0 +1,7 @@
+package com.example.android.jokeproviderlib;
+
+public class JokeProvider {
+    public static String getJoke() {
+        return "This is totally a funny joke";
+    }
+}
