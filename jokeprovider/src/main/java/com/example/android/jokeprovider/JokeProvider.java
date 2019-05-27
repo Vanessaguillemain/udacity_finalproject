7cd771a1b6d1fe956dee568691a65391ea5143d9
vanessa <vanessa.guillemain@gmail.com>
2019-05-27 15:10:25 +0200

    Change some typos and strings

--- jokeprovider/src/main/java/com/example/android/jokeprovider/JokeProvider.java
+++ jokeprovider/src/main/java/com/example/android/jokeprovider/JokeProvider.java
@@ -1,9 +1,6 @@
 package com.example.android.jokeprovider;
 
 public class JokeProvider {
-    public static String getJoke() {
-        return "This is totally a funny joke";
-    }
     public static String getJokeForEndpoints() {
         return "This is totally a funny joke for Endpoints";
     }
