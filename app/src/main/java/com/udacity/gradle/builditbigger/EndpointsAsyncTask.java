59c77f8b2b258f190852a9a4e561db03541dae2b
vanessa <vanessa.guillemain@gmail.com>
2019-06-05 11:21:40 +0200

    Put URL hardcoded in BuildConfig

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -27,7 +27,7 @@ class EndpointsAsyncTask extends AsyncTask<String, Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl(BuildConfig.URL)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
